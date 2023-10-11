e1fa09d8babe8efed4bc823e225724f457538913
Noam Gat <noamgat@gmail.com>
2023-10-11 13:10:29 +0300

    Also exporting build_transformers_prefix_allowed_tokens_fn() for transformers integration

--- lmformatenforcer/__init__.py
+++ lmformatenforcer/__init__.py
@@ -3,7 +3,8 @@ __all__ = ['CharacterLevelParser',
            'RegexParser', 
            'JsonSchemaParser',
            'TokenEnforcer', 
-           'generate_enforced']
+           'generate_enforced',
+           'build_transformers_prefix_allowed_tokens_fn']
 
 from .characterlevelparser import CharacterLevelParser, StringParser
 from .regexparser import RegexParser
@@ -11,7 +12,7 @@ from .jsonschemaparser import JsonSchemaParser
 from .tokenenforcer import TokenEnforcer
 
 try:
-    from .transformerenforcer import generate_enforced
+    from .transformerenforcer import generate_enforced, build_transformers_prefix_allowed_tokens_fn
 except Exception as e:
     import logging
-    logging.warning(f"Could not import generate_enforced(). Transformers-based functionality will not be available. Details: {e}")
+    logging.warning(f"Could not import transformerenforcer. Transformers-based functionality will not be available. Details: {e}")
