d4e3c25336aed437cd39a7b840681886d4e1af99
aerdem4 <ahmeterd4@gmail.com>
2024-02-12 11:55:17 +0300

    Add TRT-LLM example

--- lmformatenforcer/integrations/trtllm.py
+++ lmformatenforcer/integrations/trtllm.py
@@ -1,24 +1,28 @@
 import math
-from typing import List, Optional
+from typing import List, Optional, Tuple
 import torch
 from transformers import PreTrainedTokenizerBase
-from lmformatenforcer import CharacterLevelParser, TokenEnforcer, FormatEnforcerAnalyzer
-from lmformatenforcer.integrations.transformers import build_token_enforcer_tokenizer_data
+from lmformatenforcer import CharacterLevelParser, FormatEnforcerAnalyzer
+from lmformatenforcer.tokenenforcer import TokenEnforcer, TokenEnforcerTokenizerData
 
 
 class TRTLLMLogitsProcessor:
-    def __init__(self, token_enforcer: TokenEnforcer, analyze):
+    def __init__(self, token_enforcer: TokenEnforcer, eos_token_id, analyze):
         self.token_enforcer = token_enforcer
         self.analyzer = FormatEnforcerAnalyzer(token_enforcer) if analyze else None
         self.mask: Optional[torch.Tensor] = None
         self.mask_val = -math.inf
+        self.eos_token_id = eos_token_id
+
+    def _trim(self, input):
+        return [x for x in input.tolist() if x != self.eos_token_id]
 
     def __call__(self, step: int, batch_input_ids: List[List[int]], logits: torch.Tensor) -> torch.Tensor:
         for idx in range(len(batch_input_ids)):
             if self.analyzer:
-                self.analyzer.report_raw_logits(batch_input_ids[idx], logits.tolist())
+                self.analyzer.report_raw_logits(batch_input_ids[idx], logits[idx].tolist())
 
-            allowed_tokens = self.token_enforcer.get_allowed_tokens(batch_input_ids[idx])
+            allowed_tokens = self.token_enforcer.get_allowed_tokens(self._trim(batch_input_ids[idx]))
 
             if self.mask is not None:
                 self.mask.fill_(self.mask_val)
@@ -31,12 +35,34 @@ class TRTLLMLogitsProcessor:
         return logits
 
 
+def _build_regular_tokens_list(tokenizer) -> List[Tuple[int, str, bool]]:
+    token_0 = [tokenizer.encode("0")[-1]]
+    regular_tokens = []
+    vocab_size = tokenizer.tokenizer.vocab_size
+    for token_idx in range(vocab_size):
+        if token_idx in tokenizer.tokenizer.all_special_ids:
+            continue
+        # We prepend token 0 and skip the first letter of the result to get a space if the token is a start word.
+        tensor_after_0 = torch.tensor(token_0 + [token_idx], dtype=torch.long)
+        decoded_after_0 = tokenizer.decode(tensor_after_0)[1:]
+        decoded_regular = tokenizer.decode(token_0)
+        is_word_start_token = len(decoded_after_0) > len(decoded_regular)
+        regular_tokens.append((token_idx, decoded_after_0, is_word_start_token))
+    return regular_tokens
+
+
 def build_trtllm_logits_processor(tokenizer: PreTrainedTokenizerBase,
                                   character_level_parser: CharacterLevelParser,
                                   analyze: bool = False) -> TRTLLMLogitsProcessor:
     """
     Build logits processor for feeding it into generate function (use_py_session should be True)
     """
-    tokenizer_data = build_token_enforcer_tokenizer_data(tokenizer)
+    regular_tokens = _build_regular_tokens_list(tokenizer)
+
+    def _decode(tokens: List[int]) -> str:
+        tensor = torch.tensor(tokens, dtype=torch.long)
+        return tokenizer.decode(tensor)
+
+    tokenizer_data = TokenEnforcerTokenizerData(regular_tokens, _decode, tokenizer.eos_token_id)
     token_enforcer = TokenEnforcer(tokenizer_data, character_level_parser)
-    return TRTLLMLogitsProcessor(token_enforcer, analyze)
+    return TRTLLMLogitsProcessor(token_enforcer, tokenizer.eos_token_id, analyze)
