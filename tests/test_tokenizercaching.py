4917d643f130ce6ea9d32547179fe94833cdabaa
Noam Gat <noamgat@gmail.com>
2024-02-17 23:41:48 +0200

    WIP: Faster algo

--- tests/test_tokenizercaching.py
+++ tests/test_tokenizercaching.py
@@ -5,14 +5,13 @@ from lmformatenforcer.tokenizerprefixtree import JsonFreetextTokenCache
 def test_json_freetext_cache():
     token_to_str = {}
     cache = JsonFreetextTokenCache()
-    test_length = 20
-    cache.max_allowed_token_len = test_length
+    test_length = 200
     def _register_token(token_idx: int, token_str: str):
         token_to_str[token_idx] = token_str
         cache.add_token(token_str, token_idx)
     for i in range(1, test_length):
         _register_token(i, "a" * i)
-        _register_token(i + cache.max_allowed_token_len, "a" * i + '"')
+        _register_token(i + test_length, "a" * i + '"')
     cache.freeze()
     for min_remaining in range(1, test_length):
         for max_length in range(min_remaining, test_length):
