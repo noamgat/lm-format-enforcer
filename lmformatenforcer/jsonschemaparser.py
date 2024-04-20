f4d6496d36af02fa82f397a49a89665015fc02a7
Noam Gat <noamgat@gmail.com>
2024-04-20 09:08:08 +0300

    Improved JSONSchemaParser performance (#93)
    * Improving backslash escaping performance, based on github discussion
    
    * Taking the finished parser's last parsed string before discarding it

--- lmformatenforcer/jsonschemaparser.py
+++ lmformatenforcer/jsonschemaparser.py
@@ -91,6 +91,15 @@ class JsonSchemaParser(CharacterLevelParser):
                 option_json_schema_parsers.append(option_parser)
             return UnionParser(option_json_schema_parsers)
 
+        # For some performance optimizations to work, we want to make sure we don't leave irrelevant
+        # objects at the top of the stack, which we know will be passed over next timestep
+        new_object_stack = updated_parser.object_stack
+        while new_object_stack and new_object_stack[-1].can_end() and new_object_stack[-1].get_allowed_characters() == '':
+            finished_receiver = new_object_stack[-1]
+            if isinstance(finished_receiver, StringParsingState):
+                updated_parser.last_parsed_string = finished_receiver.parsed_string
+            del new_object_stack[-1]
+
         return updated_parser
 
     def get_allowed_characters(self) -> str:
