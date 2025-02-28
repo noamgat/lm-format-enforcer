88ae44aa656cc082a58e4413d462b523033c5ade
Hichem Ammar Khodja <hichem5696@gmail.com>
2025-02-28 18:02:24 +0100

    MultiChoicesParser implemented and tested
    - Stress tested with a set of 10 million choices which the parser passed. The generation process was quick.

--- lmformatenforcer/multichoicesparser.py
+++ lmformatenforcer/multichoicesparser.py
@@ -1,14 +1,13 @@
 from __future__ import annotations
 from .characterlevelparser import CharacterLevelParser, CharacterLevelParserConfig
-from multi_choices_parser import MultiChoicesParser as MCP, end_symb
+from multi_choices_parser import MultiChoicesParser as MCP
 
 class MultiChoicesParser(CharacterLevelParser):
     def __init__(self, list_of_choices : list[list[str]] | None):
         """
         NOTE: This parser is based on the multi-choices-parser package: https://github.com/HichemAK/multi-choices-parser.
-        
-        
-        A efficient incremental parser for multi-choice grammars, which are defined as grammars of the form:
+
+        A efficient incremental parser for multi-choice grammars. They are defined as grammars of the form:
 
         start: list1 list2 ... listn
 
@@ -18,9 +17,9 @@ class MultiChoicesParser(CharacterLevelParser):
 
         ...
         
-        listm: choicem_1 | choicem_2 | ... | choicem_km
+        listn: choicen_1 | choicen_2 | ... | choicen_km
 
-        where choicex_y are all literals (strings) and can possibly be empty
+        where choicex_y is a sequence of integers and can possibly be empty
 
         Example:
         start: det noun
@@ -28,17 +27,11 @@ class MultiChoicesParser(CharacterLevelParser):
         det: "the " | "an " | "a " | ""
 
         noun: "orange" | "apple" | "banana"
-
-        This was particularly optimized when the size of the lists of choices is 
-        very large (up to order of millions), which can be helpful when a large number 
-        of answers is possible given some prompt. For example, this can be used
-        to represent entities preceeded (or not) by a determinent. 
         
-        Initialize the parser using a list of choices (a list of lists) which correspond 
-        to the lists introduced in the grammar above.
-
-        Args:
-            list_of_choices (list[list[str]] | None): List of choices. If None, the grammar will match an empty string exclusively.
+        This was particularly optimized when the size of the lists of choices is 
+        very large (up to order of millions), which can be helpful
+        to represent entities preceeded (or not) by a determinent. 
+        For example, in Wikipedia, there are around 7 million entities.
         """
         if list_of_choices is not None:
             self.parser = MCP(list_of_choices)
@@ -55,7 +48,7 @@ class MultiChoicesParser(CharacterLevelParser):
         return copy
 
     def get_allowed_characters(self) -> str:
-        return ''.join(x for x in self.parser.next() if x is not end_symb)
+        return ''.join(x for x in self.parser.next() if x is not self.parser.end_symb)
 
     def can_end(self) -> bool:
         return self.parser.end_symb in self.parser.next() or self.parser.finished
